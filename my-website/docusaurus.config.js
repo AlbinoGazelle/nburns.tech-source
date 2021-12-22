98097b1580ae8447420088b0ff368034f5c96807
AlbinoGazelle <nburns9922@gmail.com>
2021-12-22 11:14:43 -0800

    add gtag analytics for testing

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -52,6 +52,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+            trackingID: 'G-W0WFTF9FQH',
+            anonymizeIP: true,
+          },
       }),
     ],
   ],
