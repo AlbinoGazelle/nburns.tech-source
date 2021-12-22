48693bef2c72c9e625a911711f0fa911a1e82b5f
AlbinoGazelle <nburns9922@gmail.com>
2021-12-22 13:15:48 -0800

    rename from Nathans Website to Nathans Blog

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Nathan\'s Website',
+  title: 'Nathan\'s Blog',
   tagline: 'Welcome',
   url: 'https://albinogazelle.github.io',
   baseUrl: '/',
