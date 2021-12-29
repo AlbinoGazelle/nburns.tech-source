e474b2f3f38c924e5806492e4d014636e3ec1fd7
AlbinoGazelle <nburns9922@gmail.com>
2021-12-28 23:33:47 -0800

    update homepage

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -146,7 +146,7 @@ const config = {
             title: 'More',
             items: [
                 {
-                    label: 'Contact Me',
+                    label: 'Contact',
                     to: '/contact',
                 },
               {
