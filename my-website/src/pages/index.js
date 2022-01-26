8a6c8074f24c99a4c57158824eba2d1279a2eb14
AlbinoGazelle <nburns9922@gmail.com>
2022-01-25 23:03:50 -0800

    fix meta tag

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
   return (
     <Layout
       title={`nburns.tech`}
-      description="Description will go into a meta tag in <head />">
+      description="Hacker, Reader, Hiker, and Friend.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
